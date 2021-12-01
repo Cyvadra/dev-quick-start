d95e0cb507d9590439a8153781d63e54febb176c
dengjie <dj@lifesdk.com>
2021-12-01 11:54:47 +0800

    bugfix

--- dev-docker/build.sh
+++ dev-docker/build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-mkdir /home/minikube/workspace
-minikube start --mount-string="/home/minikube/workspace:/workspace"
-
 docker build . -f Dockerfile \
 	--build-arg ENV_ENVIRONMENT_TARGET=$ENV_ENVIRONMENT_TARGET \
 	--build-arg ENV_CONSUL_HOST=$ENV_CONSUL_HOST \
